7cef24c987edf7e706a426f5accfe5739363f9db
MauricioGomez <109110322+Marter012@users.noreply.github.com>
2024-01-03 20:41:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,44 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('navbar-change');
     }
 })
+
+const sectionSlider = document.getElementById("section-slider");
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+let activeSlide = 0;
+
+function setBgToBody() {
+    sectionSlider.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+};
+
+setBgToBody();
+
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove('active'));
+
+    slides[activeSlide].classList.add('active');
+};
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++;
+
+    if(activeSlide > slides.length - 1) {
+        activeSlide = 0;
+    };
+
+    setBgToBody();
+    setActiveSlide();
+});
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--;
+
+    if(activeSlide < 0) {
+        activeSlide = slides.length -1;
+    };
+
+    setBgToBody();
+    setActiveSlide();
+});
